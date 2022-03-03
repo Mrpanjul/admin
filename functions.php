94a28b204f0bca0123dae14f638d45980e3f0b82
Mrpanjul <57435185+Mrpanjul@users.noreply.github.com>
2022-03-03 17:39:25 +0700

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,97 @@
+<?php
+
+
+
+//INI PERLU DI HALAMAN VIOS.PHP DAN TRANSAKSI.PHP
+
+// username
+
+$nama = "PANJUL";
+
+// saldo kemenangan dan saldo utama
+$saldo = "55.500.000";
+
+// periode pasaran
+$periode = "HONGKONG";
+
+//periode nomor
+$periodenm = "1658";
+
+
+// saldo deposit
+$setor = "15.000";
+
+
+//waktu result
+$time1 = "23:01:00";
+
+//waktu betting
+$time2 = "11:31:00";
+
+//waktu depo
+$time3 = "10:12:00";
+
+
+?>
+
+
+<?php 
+
+// ini perlu di halaman WD
+//nama bank
+
+$namabnk = "BRI";
+
+// nama rekening
+$namarek = "BUDI HANDOKO";
+
+// NO rekening 4 angka terakhir saja
+$norek = "1481-8470-****";
+
+
+?>
+
+
+<?php 
+
+// INI PERLU DI HALAMAN WIN.PHP
+
+
+//saldo menang2d
+$win2d = "500.000";
+
+// saldo menang3d
+$win3d = "5.000.000";
+
+// saldo menang4d
+$win4d = "50.000.000";
+
+//taruhan betting
+$taruhan = "5000";
+
+//angka result 2d
+$tebak2d = "78";
+
+
+//angka result 3d
+$tebak3d = "678";
+
+//angka result 4d
+$tebak4d = "1678"
+
+
+?>
+
+
+<?php 
+
+// INI PERLU UNTUK HALAMAN BELI.PHP
+
+
+//hasil keluaran jika belum result ubah menggunakan tanda -
+$hasilklr = "-";
+
+//total bayar pasangan betting
+$beting = "15.000";
+
+?>
